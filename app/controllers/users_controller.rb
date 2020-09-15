8b87aaa3dd867944310f96b23f0bd310b9e44927
Ejo415 <ejo7220@gmail.com>
2020-09-15 16:06:04 -0400

    built login/logout methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,4 +19,31 @@ class UsersController < ApplicationController
         end
     end
 
+    get '/login' do
+        if !logged_in?
+            erb :'users/login'
+        else
+            redirect to '/ships'
+        end
+    end
+
+    post 'login' do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect to '/ships'
+        else
+            redirect to '/signup'
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.destroy
+            redirect to '/login'
+        else
+            redirect to '/'
+        end
+    end
+
 end
