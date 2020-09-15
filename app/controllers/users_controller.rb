bbdee244a91b835ebb08a5496ea40fc02bb7673c
Ejo415 <ejo7220@gmail.com>
2020-09-15 15:11:47 -0400

    created signup post and do routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if !logged_in?
+            erb :'users/signup'
+        else
+            redirect to '/ships'
+        end
+    end
+
+    post '/signup' do
+        if params[:username] == "" || params[:password] == ""
+            redirect to '/signup'
+        else
+            @user = User.new(:username => params[:username], :password => params[:password])
+            @user.save
+            session[user_id] = @user.id 
+            redirect to '/ships'
+        end
+    end
+
 end
