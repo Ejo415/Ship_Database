9d53ab0a657ca5fba1b8cad7383b49db8f883472
Ejo415 <ejo7220@gmail.com>
2020-09-16 17:18:28 -0400

    added post for shp, and edit methods

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -7,8 +7,30 @@ class ShipsController < ApplicationController
     end
 
     get '/ships/new' do
-        erb :'ships/new/'
+        redirect_if_not_logged_in
+        erb :'ships/new'
     end
+
+    post '/ships' do
+        redirect_if_not_logged_in
+        #binding.pry
+        Ship.create(name: params[:name], klass: params[:klass], registry: params[:registry], affiliation: params[:affiliation])
+        redirect to '/ships'
+    end
+
+    get '/ships/:id' do
+        redirect_if_not_logged_in
+        @ship = Ship.find_by_id(params[:id])
+        erb :'ships/show'
+    end
+
+    get '/ships/:id/edit' do
+        redirect_if_not_logged_in
+        @ship = Ship.find_by_id(params[:id])
+        erb :"ships/edit"
+        
+    end
+
 
 
 
