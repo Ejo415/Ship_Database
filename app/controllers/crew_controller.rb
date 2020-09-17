0de38cdc27f2efa63a94dcec1de7fdb18975ee2b
Ejo415 <ejo7220@gmail.com>
2020-09-17 12:41:50 -0400

    repurposed ship controller code for base

--- app/controllers/crew_controller.rb
+++ app/controllers/crew_controller.rb
@@ -1,3 +1,47 @@
 class CrewController < ApplicationController
+    
+    get '/crew' do
+        redirect_if_not_logged_in
+        @ships = Ship.all
+        erb :'crew/index'
+    end
+
+    get '/crew/new' do
+        redirect_if_not_logged_in
+        erb :'crew/new'
+    end
+
+    post '/crew' do
+        redirect_if_not_logged_in
+        #binding.pry
+        Crew.create(name: params[:name], rank: params[:rank], species: params[:species], affiliation: params[:affiliation])
+        redirect to '/crew'
+    end
+
+    get '/crew/:id' do
+        redirect_if_not_logged_in
+        @crew = Crew.find_by_id(params[:id])
+        erb :'crew/show'
+    end
+
+    get '/crew/:id/edit' do
+        redirect_if_not_logged_in
+        @crew = Crew.find_by_id(params[:id])
+        erb :"crew/edit"
+    end
+
+    patch '/crew/:id' do
+        redirect_if_not_logged_in
+        crew = Crew.find_by_id(params[:id])
+        crew.update(name: params[:name], rank: params[:rank], species: params[:species], affiliation: params[:affiliation])
+        redirect "/crew/#{crew.id}"
+    end
+
+    delete '/crew/:id' do
+        redirect_if_not_logged_in
+        crew = Crew.find_by_id(params[:id]) 
+        crew.destroy
+        redirect '/crew'
+    end
 
 end
