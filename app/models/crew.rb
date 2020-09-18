6ce02b7d2ee7919311cd875286cc658ae1d862ce
Ejo415 <ejo7220@gmail.com>
2020-09-18 16:08:01 -0400

    added field validation

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -1,3 +1,7 @@
 class Crew < ActiveRecord::Base
-    belongs_to :ships
+    belongs_to :ship
+    validates :name, presence: true
+    validates :rank, presence: true
+    validates :species, presence: true
+    validates :affiliation, presence: true
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,8 @@
 class Ship < ActiveRecord::Base
     has_many :crews
     belongs_to :user
+    validates :name, presence: true
+    validates :klass, presence: true
+    validates :registry, presence: true
+    validates :affiliation, presence: true
 end
