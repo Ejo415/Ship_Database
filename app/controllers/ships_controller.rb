750465427d1b1487c0af87eecaeb11cfbd3c7910
Ejo415 <ejo7220@gmail.com>
2020-09-18 09:58:32 -0400

    added user id and @crew_id to params

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -2,25 +2,28 @@ class ShipsController < ApplicationController
 
     get '/ships' do
         redirect_if_not_logged_in
-        @ships = Ship.all
+        #binding.pry
+        @ships = current_user.ships
         erb :'ships/index'
     end
 
     get '/ships/new' do
         redirect_if_not_logged_in
+        @crew = Crew.all
         erb :'ships/new'
     end
 
     post '/ships' do
         redirect_if_not_logged_in
         #binding.pry
-        Ship.create(name: params[:name], klass: params[:klass], registry: params[:registry], affiliation: params[:affiliation])
+        Ship.create(name: params[:name], klass: params[:klass], registry: params[:registry], affiliation: params[:affiliation], user_id: current_user.id)
         redirect to '/ships'
     end
 
     get '/ships/:id' do
         redirect_if_not_logged_in
         @ship = Ship.find_by_id(params[:id])
+        @crew = Crew.all
         erb :'ships/show'
     end
 
@@ -33,8 +36,10 @@ class ShipsController < ApplicationController
     patch '/ships/:id' do
         redirect_if_not_logged_in
         ship = Ship.find_by_id(params[:id])
+        if current_user.id = ships.user_id
         ship.update(name: params[:name], klass: params[:klass], registry: params[:registry], affiliation: params[:affiliation])
         redirect "/ships/#{ship.id}"
+        end
     end
 
     delete '/ships/:id' do
