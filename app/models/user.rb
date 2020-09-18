db50bec7b64c677d6fd3d202a7072490add6023d
Ejo415 <ejo7220@gmail.com>
2020-09-18 18:09:21 -0400

    added password requirement upon sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
     validates :username, presence: true
+    validates :password, presence: true    
     validates :username, uniqueness: true
     has_many :ships
     #has_many :crew, through: :ships
